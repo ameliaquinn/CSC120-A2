a6a359c4f3fe3185d2704c4100e691e8c24d8fa5
Amelia Quinn <ameliaquinn@Amelias-MacBook-Air-5.local>
2024-09-26 10:58:43 -0400

    first commit

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,48 @@
 class ResaleShop:
 
     # What attributes will it need?
+    inventory: list 
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self):
+        self.inventory = []
+
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    # GOAL: add c to self.inventory
+    def buy(self, c: str):
+            self.inventory.append(c)
+    
+    def sell(self, itemID: int):
+         if itemID < len(self.inventory):
+              del self.inventory[itemID]
+              print("Item", itemID, "successfully sold!")
+         else: 
+              print("Item", itemID, "not found. Please try again!")
+    def print_inventory(self):
+         if self.inventory:
+              for i in self.inventory:
+                  print("Item ID:", i) 
+                  print("Name:", self.inventory[i])
+              else: 
+                   print("No inventory to display!")
+              
+            
+              
+
+         
+
+def main():
+     myShop: ResaleShop = ResaleShop()
+     print("There are", len(myShop.inventory), "items in stock.")
+     c = "My Awesome Computer"
+     myShop.buy(c)
+     print("There are now", len(myShop.inventory), "items in stock.")
+     myShop.sell(1)
+
+
+     
+if __name__=="__main__":
+    main()
--- reflection.md
+++ reflection.md
@@ -1,3 +1,5 @@
 Use this file to record your reflection on this assignment. 
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I think I understood the general concept of the assignment and actually coding really helped me to understand the difference between procedural and object-oriented programming. It worked to just focus on the objectives in the rubric & to come up with a brief pseudocode plan before I began. However, I wish I'd started earlier to give myself more time and used my resources... I would tell someone taking this course in the future to take advantage of all the resources you have available to you (TA's, office hours, class time, etc. etc.) because they will save you so much work and time!
